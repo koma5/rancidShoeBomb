c263b4b05e7898a8ed17340ffbc97ca5d752aea7
Marco Koch <marco@5th.ch>
2017-12-15 21:28:01 +0100

    some static data (not much more)

--- startWithSomething.js
+++ startWithSomething.js
@@ -1,9 +1,19 @@
 const express = require('express', '4.16.2')
 const app = express()
 
+var router = express.Router();
 
 message = {shout: 'rancidShoeBomb!!'};
 
-app.get('/', (req, res) => res.send(message))
+app.use('/', router);
+
+router.use(function(req, res, next) {
+	console.log('bomb on ' + req.url);
+	next();
+});
+
+
+router.get('/', (req, res) => res.send(message))
+router.get('/landfills', (req, res) => res.send({resultset:[{name:'freshkills'}, {name:'Vorderthal common dump'}]}))
 
 app.listen(3000, () => console.log('throwing rancidShoeBombs on port 3000'))
