a394deec04224d0d42a81b67f23580cf2db46aaf
Marco Koch <marco@5th.ch>
2019-03-27 19:41:47 +0000

    switched from angular to vue

--- webapp/app.js
+++ webapp/app.js
@@ -1,10 +1,10 @@
-var app = angular.module("rancidShoeBomb", []);
-
-
-app.controller("rancidController", function($scope, $http){
-
-  $http.get('http://127.0.0.1:3000/landfills').then(function(response){
-    $scope.landfills = response.data;
-  });
-
-});
+var app = new Vue({
+	el: '#app',
+	data: {
+		message: "Hello fillers of land",
+		landfills: [1,2]
+	},
+	mounted() {
+		axios.get('http://127.0.0.1:3000/landfills').then(response => (this.landfills = response.data))
+	}
+})
