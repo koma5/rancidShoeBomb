4baf141cf6594303113672b87ed3d569ad737515
Marco Koch <marco@5th.ch>
2019-03-27 22:25:23 +0000

    add landfill though html form; very crude CRUD

--- api/server.js
+++ api/server.js
@@ -23,6 +23,7 @@ app.use(bodyParser.json());
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header('Access-Control-Allow-Headers', 'Access-Control-*, Origin, X-Requested-With, Content-Type, Accept, Authorization');
 	rancidLog(" bomb on " + req.url);
 	next();
 });
--- webapp/app.js
+++ webapp/app.js
@@ -2,9 +2,19 @@ var app = new Vue({
 	el: '#app',
 	data: {
 		message: "Hello fillers of land",
-		landfills: [1,2]
+		landfills: []
 	},
 	mounted() {
 		axios.get('http://127.0.0.1:3000/landfills').then(response => (this.landfills = response.data))
+	},
+	methods: {
+
+		addLandfill: function(e) {
+			if(e.target.value !== '') {
+				this.landfills.push({'name': e.target.value});
+				axios.post('http://127.0.0.1:3000/landfills', {'name': e.target.value})
+				e.target.value = '';
+			}
+		}
 	}
 })
