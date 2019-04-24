e008526fd983c49804fc7aed78a74d761017264a
Marco Koch <marco@5th.ch>
2019-04-24 20:06:08 +0100

    styling LandfillDetail according to notes/mock.jpeg

--- rancidui/src/components/LandfillDetail.vue
+++ rancidui/src/components/LandfillDetail.vue
@@ -1,21 +1,39 @@
 <template>
     <section>
-            filly land of {{ $route.params.id }} coming soon!
         <article>
-            <h1>{{item.name}}</h1>
-            opened: {{item.opened}}
-            dumpees: {{item.dumplingCount}}
+            <div class="depiction">
+                <h1>{{item.name}}</h1>
+            </div>
+
+            <div class="details">
+                <ul>
+                    <li>opened: {{item.opened}}</li>
+                    <li>dumpees: {{item.dumplingCount}}</li>
+                    <li>blahh: {{item.dumplingCount}}</li>
+                    <li>more: {{item.dumplingCount}}</li>
+                    <li>Area: {{item.dumplingCount}}</li>
+                    <li>dumpees: {{item.dumplingCount}}</li>
+                    <li v-on:click="showDumplings = !showDumplings">show dumpees below ▶▼</li>
+                </ul>
+            </div>
+
+            <div v-if="showDumplings" class="dumplings">
+                <TilingGrid apiResource="dumplings" />
+            </div>
         </article>
     </section>
 </template>
 
 <script>
+import TilingGrid from './TilingGrid.vue';
 
 export default {
     name: 'LandfillDetail',
+    components: { TilingGrid },
     data() {
         return {
-            item: {}
+            item: {},
+            showDumplings: false
         }
     },
 
@@ -35,16 +53,46 @@ export default {
 </script>
 
 
-<style>
-article {
-    width: 300px;
-    height: 300px;
-    background-color: lightgray;
-    box-shadow: 1px 1px;
-    padding: 10px;
-    margin-left: auto;
-    margin-right: auto;
+<style scoped>
+
+section {
+    width: 660px;
+    height: 100%;
+    margin: auto;
 }
 
+article {
+    display: grid;
+    grid-template-columns: 300px auto;
+    grid-template-rows: auto auto;
+    grid-gap: 40px;
+}
+
+article h1 {
+    text-transform: uppercase;
+    font-size: 3.5em;
+    word-wrap: break-word;
+    margin: 0;
+}
+
+.depiction {
+    width: 270px;
+    height: 270px;
+    background-color: lightgray;
+    box-shadow: 1px 1px;
+    padding: 30px;
+}
+
+.details ul {
+  list-style: none;
+}
+
+.details {
+    text-align: left;
+}
+
+.dumplings {
+    grid-column: 1 / span 2;
+}
 
 </style>
